384aa4d6a541b27b1addd449e473feab554118f3
j4n7 <garciagutierrez@gmail.com>
2025-03-07 00:54:49 +0100

    Deploy website - based on HEAD

--- assets/js/9200a86a.1db54c0b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrunicarpg=self.webpackChunkrunicarpg||[]).push([[478],{2842:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/runicarpg/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/acecf23e.dcac3257.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrunicarpg=self.webpackChunkrunicarpg||[]).push([[1903],{1912:r=>{r.exports=JSON.parse('{"blogBasePath":"/runicarpg/blog","blogTitle":"Blog","authorsListPath":"/runicarpg/blog/authors"}')}}]);
